7a3f47f1a47c3d3f0ece40b11c1a245d804561dd
amir nikmaram <am.nikmaram@hotmail.com>
2019-11-12 08:50:16 +0330

    end session 2

--- TopLearn.Web/Startup.cs
+++ TopLearn.Web/Startup.cs
@@ -15,6 +15,7 @@ namespace TopLearn.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,8 @@ namespace TopLearn.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvcWithDefaultRoute();
+            app.UseSpaStaticFiles();
 
             app.Run(async (context) =>
             {
